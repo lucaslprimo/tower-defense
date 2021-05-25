a75e76ae08eae1f3c066be7a7ebb4e060a965de6
lucaslprimo <lucaslprimo@gmail.com>
2021-05-25 19:56:16 -0300

    Adding BuildSpaces and Props

--- Assets/Scripts/BuildingSpace.cs
+++ Assets/Scripts/BuildingSpace.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class BuildingSpace : MonoBehaviour
 {
+    [SerializeField] Renderer rend;
+    [SerializeField] Color highLightColor;
     public bool isBuildable;
+    private Color startColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startColor = rend.materials[1].color;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (isBuildable)
+        {
+            rend.materials[1].color = highLightColor;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (isBuildable)
+        {
+            rend.materials[1].color = startColor;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isBuildable)
+        {
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Props") && isBuildable)
+        {
+            isBuildable = false;
+        }
     }
 }
